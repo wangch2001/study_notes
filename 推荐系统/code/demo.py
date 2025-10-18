39b43caa8190ef29e78ebee702c6b93b41fa1c9f
wangch <786773891@qq.com>
2025-10-18 15:18:06 +0800

    Update demo.py

--- 推荐系统/code/demo.py
+++ 推荐系统/code/demo.py
@@ -1,21 +1,4 @@
-
-def partition(arr, low, high):
-    pivot = arr[high]
-    i = low - 1
-    for j in range(low, high):
-        if arr[j] <= pivot:
-            i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-    arr[i+1], arr[high] = arr[high], arr[i+1]
-    return i + 1
-
-def quicksort_inplace(arr, low, high):
-    if low < high:
-        pi = partition(arr, low, high)
-        quicksort_inplace(arr, low, pi - 1)
-        quicksort_inplace(arr, pi + 1, high)
-
-nums = [3,6,8,10,1,2,1]
-print("排序前：", nums)
-quicksort_inplace(nums, 0, len(nums)-1)
-print("排序后：", nums)
+path = [1, 2, 3, 4, 5]
+path.reverse()
+print(path)
+[5, 4, 3, 2, 1]
