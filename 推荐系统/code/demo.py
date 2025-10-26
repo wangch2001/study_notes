d6644ad79ed37f91521684b42483f5597a7de815
wangch <786773891@qq.com>
2025-10-26 17:03:17 +0800

    update

--- 推荐系统/code/demo.py
+++ 推荐系统/code/demo.py
@@ -1,23 +1,47 @@
-class ListNode():
-    def __init__(self, val = 0, next = None):
+from collections import deque
+
+
+class TreeNode():
+    def __init__(self, val=0, left=None, right=None):
         self.val = val
-        self.next = next
+        self.left = left
+        self.right = right
 
-def build_ListNode(arr):
-    dummy = ListNode()
-    cur = dummy
-    for x in arr:
-        cur.next = ListNode(x)
-        cur = cur.next
-    return dummy.next
+def build_tree(values):
+    if not values or values[0] == "null":
+        return None
+    root = TreeNode(int(values[0]))
+    q = deque([root])
+    i = 1
+    while q and i < len(values):
+        node = q.popleft()
+        if values[i] != "null":
+            node.left = TreeNode(values[i])
+            q.append(node.left)
+        i += 1
+        if i < len(values) and values[i] != "null":
+            node.right = TreeNode(values[i])
+            q.append(node.right)
+        i += 1
+    return root
 
-def print_ListNode(head):
-    cur = head
-    while cur:
-        print(cur.val, end = " ")
-        cur = cur.next
-    print()
 
-arr = list(map(int, input().split()))
-head = build_ListNode(arr)
-print_ListNode(head)
+def print_tree(root):
+    if not root:
+        print("None")
+        return
+    q = deque([root])
+    res = []
+    while q:
+        node = q.popleft()
+        if node:
+            res.append(str(node.val))
+            q.append(node.left)
+            q.append(node.right)
+        else:
+            res.append("null")
+    print(" ".join(res))
+
+values = input().split()
+root = build_tree(values)
+print_tree(root)
